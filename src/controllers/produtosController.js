e367d5fb48c7161841489d2b94b1fdeebeb8ed34
Gabriel Fonseca <gabriel.gfonseca2007@gmail.com>
2026-03-12 15:22:23 -0300

    Fix: para o exercicio 9 funcionar.

--- src/controllers/produtosController.js
+++ src/controllers/produtosController.js
@@ -1,29 +1,11 @@
 let produtos = [];
 let nextId = 1;
 
-function listar(req, res) {}
-
-function buscar(req, res) {}
-
-function criar(req, res) {}
-
-function atualizar(req, res) {}
-
-function remover(req, res) {}
-
-module.exports = {
-  listar,
-  buscar,
-  criar,
-  atualizar,
-  remover
-};
-
 function listar(req, res) {
   return res.status(200).json(produtos);
 }
 
-function buscarPorId(req, res) {
+function buscar(req, res) {
   const { id } = req.params;
   const produto = produtos.find(p => p.id === Number(id));
 
@@ -33,3 +15,9 @@ function buscarPorId(req, res) {
 
   return res.status(200).json(produto);
 }
+
+function criar(req, res) {}
+function atualizar(req, res) {}
+function remover(req, res) {}
+
+module.exports = { listar, buscar, criar, atualizar, remover };
